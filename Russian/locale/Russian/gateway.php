7c236fb6f7bd79ed737375835edbfc261e44b7c5
ovasclub <ovas.club@mail.ru>
2018-10-20 15:18:18 +0600

    Create gateway.php

---
+++ Russian/locale/Russian/gateway.php
@@ -0,0 +1,76 @@
+<?php
+// Number to word function
+$locale['gateway_001'] = "Один";
+$locale['gateway_002'] = "Два";
+$locale['gateway_003'] = "Три";
+$locale['gateway_004'] = "Четыре";
+$locale['gateway_005'] = "Пять";
+$locale['gateway_006'] = "Шесть";
+$locale['gateway_007'] = "Семь";
+$locale['gateway_008'] = "Восемь";
+$locale['gateway_009'] = "Девять";
+$locale['gateway_010'] = "Десять";
+$locale['gateway_011'] = "Одинадцать";
+$locale['gateway_012'] = "Двенадцать";
+$locale['gateway_013'] = "Тринадцать";
+$locale['gateway_014'] = "Четырнадцать";
+$locale['gateway_015'] = "Пятнадцать";
+$locale['gateway_016'] = "Шестнадцать";
+$locale['gateway_017'] = "Семнадцать";
+$locale['gateway_018'] = "Восемнадцать";
+$locale['gateway_019'] = "Девятнадцать";
+$locale['gateway_020'] = "Десять";
+$locale['gateway_021'] = "Двадцать";
+$locale['gateway_022'] = "Тридцать";
+$locale['gateway_023'] = "Сорок";
+$locale['gateway_024'] = "Пятьдесят";
+$locale['gateway_025'] = "Шестьдесят";
+$locale['gateway_026'] = "Семдесят";
+$locale['gateway_027'] = "Восемдесят";
+$locale['gateway_028'] = "Девяносто";
+$locale['gateway_029'] = "Сто";
+$locale['gateway_030'] = "тысяча";
+$locale['gateway_031'] = "миллион";
+$locale['gateway_032'] = "миллиард";
+$locale['gateway_033'] = "триллион";
+$locale['gateway_034'] = "квадраллион";
+$locale['gateway_035'] = "квинтиллион";
+$locale['gateway_036'] = "секстиллион";
+$locale['gateway_037'] = "септиллион";
+$locale['gateway_038'] = "октиллион";
+$locale['gateway_039'] = "нониллион";
+$locale['gateway_040'] = "дециллион";
+$locale['gateway_041'] = "андециллион";
+$locale['gateway_042'] = "дуодецикллион";
+$locale['gateway_043'] = "тредецикллион";
+$locale['gateway_044'] = "куотедецикллион";
+$locale['gateway_045'] = "квиндециллион";
+$locale['gateway_046'] = "сексдециллион";
+$locale['gateway_047'] = "сертендициллион";
+$locale['gateway_048'] = "октодециллион";
+$locale['gateway_049'] = "новемдециллион";
+$locale['gateway_050'] = "вигинтиллион";
+$locale['gateway_051'] = " тысяча";
+
+$locale['gateway_052'] = "Пожайлуста включите Javascript до регистрации ";
+
+// Honeypot default Array, can be made user defined by db later with a Gateway interface that has a lot of options.
+$locale['gateway_053'] = "факс";
+$locale['gateway_054'] = "телефон";
+$locale['gateway_055'] = "адрес";
+$locale['gateway_056'] = "Е-майл";
+$locale['gateway_057'] = "имя";
+$locale['gateway_058'] = "пользователь";
+$locale['gateway_059'] = "пароль";
+
+// General
+$locale['gateway_060'] = "Сколько будет => (";
+$locale['gateway_061'] = "";
+$locale['gateway_062'] = ") = ???<br/> Дайте ответ прописью ниже";
+$locale['gateway_063'] = ") = ???<br/> Дайте ответ числами ниже";
+$locale['gateway_064'] = "Вы должны ответить на вопрос";
+$locale['gateway_065'] = "Продолжить";
+
+$locale['gateway_066'] = "Ваш ответ не правильный!";
+$locale['gateway_067'] = "Попытайтесь заново!";
+$locale['gateway_068'] = "Пожайлуста повторите попытку позже!";
