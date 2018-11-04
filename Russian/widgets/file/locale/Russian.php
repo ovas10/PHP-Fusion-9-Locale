c6cc3175c9d2881dbd8bbf7861c6a33b6020f181
ovasclub <ovas.club@mail.ru>
2018-11-04 22:55:26 +0600

    Update Russian.php

--- Russian/widgets/file/locale/Russian.php
+++ Russian/widgets/file/locale/Russian.php
@@ -1,6 +1,21 @@
 <?php
+/*-------------------------------------------------------+
+| PHP-Fusion Content Management System
+| Copyright (C) PHP-Fusion Inc
+| https://www.php-fusion.co.uk/
++--------------------------------------------------------+
+| Filename: file/locale/Russian.php
+| https://ovas.club
++--------------------------------------------------------+
+| This program is released as free software under the
+| Affero GPL license. You can redistribute it and/or
+| modify it under the terms of this license which you
+| can read by viewing the included agpl.txt or online
+| at www.gnu.org/licenses/agpl.html. Removal of this
+| copyright header is strictly prohibited without
+| written permission from the original author(s).
++--------------------------------------------------------*/
 $locale['f0100'] = "Заголовок";
-$locale['f0101'] = "Включить файл";
 $locale['f0102'] = "Пусть к файлу от корневой папки";
 $locale['f0103'] = "Сохранить файл";
 $locale['f0104'] = "Сохранить и закрыть файл";
