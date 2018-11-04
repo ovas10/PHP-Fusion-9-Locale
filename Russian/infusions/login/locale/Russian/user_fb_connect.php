51f627ec21f34f03c0203eccaefcbc3546501f8b
ovasclub <ovas.club@mail.ru>
2018-11-04 21:14:22 +0600

    Create user_fb_connect.php

---
+++ Russian/infusions/login/locale/Russian/user_fb_connect.php
@@ -0,0 +1,73 @@
+<?php
+/*-------------------------------------------------------+
+| PHP-Fusion Content Management System
+| Copyright (C) PHP-Fusion Inc
+| https://www.php-fusion.co.uk/
++--------------------------------------------------------+
+| Filename: login/locale/Russian/user_fb_connect.php
+| https://ovas.club
++--------------------------------------------------------+
+| This program is released as free software under the
+| Affero GPL license. You can redistribute it and/or
+| modify it under the terms of this license which you
+| can read by viewing the included agpl.txt or online
+| at www.gnu.org/licenses/agpl.html. Removal of this
+| copyright header is strictly prohibited without
+| written permission from the original author(s).
++--------------------------------------------------------*/
+$locale['uf_fb_connect'] = "Facebook Login";
+$locale['uf_fb_connect_desc'] = "Facebook Login Connector";
+$locale['uf_fb_connect_verify'] = "Подтверждение Facebook Login";
+
+// Driver configuration form
+$locale['uf_fb_connect_200'] = "Login Facebook с помощью Facebook SDK позволяет людям войти на вашу веб-страницу с учетными данными Фейсбука.";
+$locale['uf_fb_connect_201'] = "Настройки Facebook Login ";
+$locale['uf_fb_connect_202'] = "Facebook App ID";
+$locale['uf_fb_connect_203'] = "Секрет приложения Фейсбук";
+$locale['uf_fb_connect_204'] = "Введите ваш Facebook Developer App ID";
+$locale['uf_fb_connect_205'] = "Вам необходимо иметь действительный Facebook Client APP ID что бы использовать эту функцию входа";
+$locale['uf_fb_connect_206'] = "Если у вас нет APP ID, создайте и получите APP ID клиента Фейсбука на странице Facebook Developer.";
+$locale['uf_fb_connect_207'] = "Настройки плагина";
+$locale['uf_fb_connect_208'] = "Ширина";
+$locale['uf_fb_connect_209'] = "Ширина(px) кнопки плагина Фейсбук ";
+$locale['uf_fb_connect_210'] = "Размер кнопки";
+$locale['uf_fb_connect_211'] = "Маленький";
+$locale['uf_fb_connect_212'] = "Средний";
+$locale['uf_fb_connect_213'] = "Большой";
+$locale['uf_fb_connect_214'] = "Показывать лица друзей";
+$locale['uf_fb_connect_215'] = "Показать фотографии друзей, которые использовали этот сайт";
+$locale['uf_fb_connect_216'] = "Включить имя и фото профиля, когда пользователь подписан в Facebook";
+$locale['uf_fb_connect_217'] = "Максимальное количество рядов фото";
+$locale['uf_fb_connect_218'] = "Максимальное количество рядов для показа фото профиля";
+$locale['uf_fb_connect_219'] = "Текст кнопки";
+$locale['uf_fb_connect_220'] = "Продолжить с...";
+$locale['uf_fb_connect_221'] = "Войти с...";
+$locale['uf_fb_connect_222'] = "Включить кнопку «Выйти»";
+$locale['uf_fb_connect_223'] = "Сохранить настройки Фейсбук";
+
+// Verification responses
+$locale['uf_fb_connect_300'] = "Адресс Е-майла Входа Фейсбука проверен";
+$locale['uf_fb_connect_301'] = "Благодарим вас за подтверждение. Facebook Login теперь активирован.";
+$locale['uf_fb_connect_302'] = "Неверный адрес Е-майла.";
+$locale['uf_fb_connect_303'] = "Мы не смогли проверить адрес Е-майла. Попробуйте снова подключиться к Facebook Login.";
+$locale['uf_fb_connect_304'] = "У Кода Активации истёк срок.";
+$locale['uf_fb_connect_305'] = "Истёк срок подтверждающего Е-майла. Попробуйте снова подключиться к Facebook Login.";
+$locale['uf_fb_connect_306'] = "Неверный Код Активации.";
+$locale['uf_fb_connect_307'] = "Код активации недействителен. Попробуйте снова подключиться к Facebook Login.";
+
+// User Fields Connector
+$locale['uf_fb_connect_400'] = 'Фейсбук еще не связан с Вашим аккаунтом';
+$locale['uf_fb_connect_401'] = 'Фейсбук подключен к вашей учетной записи';
+$locale['uf_fb_connect_402'] = 'Связать Ваш аккаунт с Фейсбуком';
+$locale['uf_fb_connect_403'] = 'Отключить Ваш Аккаунт от Фейсбука';
+$locale['uf_fb_connect_404'] = "Свяжите свой аккаунт Фейсбука с аккаунтом сайта «{SITE_NAME}».";
+$locale['uf_fb_connect_405'] = "Ожидайте активацию с аккаунтом Фейсбука.<br/>Проверьте зарегистрированный на Фейсбуке Е-майл для активации по ссылке.";
+$locale['uf_fb_connect_406'] = "Вернуться вперёд";
+
+// Email
+$locale['uf_fb_connect_500'] = "{SITE_NAME} - Подтверждение Аккаунта Е-майлом";
+$locale['uf_fb_connect_501'] = "Уважаемый {USER_NAME},<br/><br/>Вы недавно пытались связать свою учетную запись Фейсбук с учетной записью пользователя {SITE_NAME} . Чтобы завершить синхронизацию учетной записи, нажмите на ссылку ниже:<br/>{LINK}<br/><br/>Мы рекомендуем вам активировать свою учетную запись в течение 3 дней после получения этого Е-майла, чтобы избежать истечения срока действия и прекращения связанных служб.<br/><br/>С уважением,<br/>{ADMIN_NAME}, {SITE_NAME}";
+$locale['uf_fb_connect_502'] = "Мы отправили письмо с подтверждением на ваш Е-майл Фейсбука.";
+$locale['uf_fb_connect_503'] = "Эта учетная запись Фейсбук связана с учетной записью.";
+$locale['uf_fb_connect_504'] = "Вы уже подключились к своему Фейсбуку";
+$locale['uf_fb_connect_505'] = "Вы успешно подключили свою учетную запись Фейсбука.";
