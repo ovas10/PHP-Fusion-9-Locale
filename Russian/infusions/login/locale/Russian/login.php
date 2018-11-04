c44e1fc6f193aa1c8e744a47936c7566a47c90a3
ovasclub <ovas.club@mail.ru>
2018-11-04 21:10:53 +0600

    Create login.php

---
+++ Russian/infusions/login/locale/Russian/login.php
@@ -0,0 +1,49 @@
+<?php
+/*-------------------------------------------------------+
+| PHP-Fusion Content Management System
+| Copyright (C) PHP-Fusion Inc
+| https://www.php-fusion.co.uk/
++--------------------------------------------------------+
+| Filename: login/locale/Russian/login.php
+| https://ovas.club
++--------------------------------------------------------+
+| This program is released as free software under the
+| Affero GPL license. You can redistribute it and/or
+| modify it under the terms of this license which you
+| can read by viewing the included agpl.txt or online
+| at www.gnu.org/licenses/agpl.html. Removal of this
+| copyright header is strictly prohibited without
+| written permission from the original author(s).
++--------------------------------------------------------*/
+/*
+ * Plugins
+ */
+$locale['login_000'] = "Вход в систему";
+$locale['login_001'] = "Система входа позволяет управлять учетными записями пользователей с помощью сторонней системы аутентификации.";
+$locale['login_002'] = "Управление входом";
+
+/*
+ * Administration
+ */
+
+$locale['login_100'] = "Драйвер входа";
+$locale['login_101'] = "Тип";
+$locale['login_102'] = "Версия";
+$locale['login_103'] = "Статус";
+$locale['login_104'] = "Конфигурация";
+$locale['login_105'] = "Установлен";
+$locale['login_106'] = "Отключен";
+$locale['login_107'] = "Установить Драйвер";
+$locale['login_108'] = "Удалить Драйвер";
+$locale['login_109'] = "Настроен";
+$locale['login_110'] = "Не настроен";
+$locale['login_111'] = "Настроить {USER_FIELD}";
+
+$locale['login_120'] = "{DRIVER_NAME} был успешно установлен";
+$locale['login_121'] = "{DRIVER_NAME} был удален.";
+$locale['login_122'] = "Предупреждение! Удаление драйверов приведет к деактивации и удалению информации из вашей учетной записи. Продолжить?";
+$locale['login_123'] = "Конфигурации нет. Проверьте настройки драйвера.";
+$locale['login_124'] = "Обратно на страницу установки Драйвера";
+$locale['login_125'] = "Файл конфигурации не найден";
+$locale['login_126'] = "Недопустимый драйвер / Неизвестная ошибка";
+$locale['login_127'] = "Конфигурация обновлена";
