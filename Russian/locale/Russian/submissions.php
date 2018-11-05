ea385529151f49dc78a1492e8601149d4e2d41f7
ovasclub <ovas.club@mail.ru>
2018-11-06 05:50:35 +0600

    Create submissions.php

---
+++ Russian/locale/Russian/submissions.php
@@ -0,0 +1,6 @@
+<?php
+$locale['article_submit'] = "Статей: %s";
+$locale['blog_submit'] = "Записей блога: %s";
+$locale['download_submit'] = "Файлов: %s";
+$locale['gallery_submit'] = "Фото: %s";
+$locale['news_submit'] = "Новостей: %s";
