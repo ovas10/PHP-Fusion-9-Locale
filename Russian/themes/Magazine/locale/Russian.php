d5c7711ec02934628b1aaf89abc28c673f69da3e
OVAS.CLUB <ovas.club@mail.ru>
2021-01-31 13:36:14 +0600

    Update Russian.php

--- Russian/themes/Magazine/locale/Russian.php
+++ Russian/themes/Magazine/locale/Russian.php
@@ -1,21 +1,5 @@
 <?php
-/*-------------------------------------------------------+
-| PHP-Fusion Content Management System
-| Copyright (C) PHP-Fusion Inc
-| https://www.php-fusion.co.uk/
-+--------------------------------------------------------+
-| Filename: Magazine/locale/Russian.php
-| https://ovas.club
-+--------------------------------------------------------+
-| This program is released as free software under the
-| Affero GPL license. You can redistribute it and/or
-| modify it under the terms of this license which you
-| can read by viewing the included agpl.txt or online
-| at www.gnu.org/licenses/agpl.html. Removal of this
-| copyright header is strictly prohibited without
-| written permission from the original author(s).
-+--------------------------------------------------------*/
-$locale['MG_description'] = 'Оффициальная тема для PHP-Fusion Andromeda 9.03';
+$locale['MG_description'] = 'Официальная тема для PHPFusion Andromeda 9.03';
 $locale['created_by']  = 'Создана';
 $locale['MG_001'] = 'Привет, ';
 // Templates
@@ -26,5 +10,5 @@ $locale['MG_201'] = 'Настройки темы обновленны.';
 $locale['MG_202'] = 'GitHub URL';
 $locale['MG_203'] = 'Facebook URL';
 $locale['MG_204'] = 'Twitter URL';
-$locale['MG_205'] = 'Google Plus URL';
+
 
